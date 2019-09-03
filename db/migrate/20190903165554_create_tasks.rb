78f1cd1f31c6a28fbd4aac9ecfe524657a7c9184
ferdox2 <ferdox2@gmail.com>
2019-09-03 11:56:21 -0500

    Generate task scaffold

---
+++ db/migrate/20190903165554_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :tittle
+      t.boolean :completed
+      t.text :description
+      t.references :list, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
