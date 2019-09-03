a417de04d4ae48cf3439b51ba6c157f7e92c9328
ferdox2 <ferdox2@gmail.com>
2019-09-03 12:26:13 -0500

    Adds devise as authentication gem

--- db/migrate/20190903165324_create_lists.rb
+++ db/migrate/20190903165324_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
       t.string :tittle
-      t.boolean :completed
+      t.boolean :completed, default: false
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/migrate/20190903165554_create_tasks.rb
+++ db/migrate/20190903165554_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :tittle
-      t.boolean :completed
+      t.boolean :completed, default: false 
       t.text :description
       t.references :list, foreign_key: true
 
