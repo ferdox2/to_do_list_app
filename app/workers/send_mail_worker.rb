6c899bb82f1cda485ba8a467b5ec88af4148b923
ferdox2 <ferdox2@gmail.com>
2019-09-08 15:21:34 -0500

    Adds AWS S3 configuration

--- app/workers/send_mail_worker.rb
+++ app/workers/send_mail_worker.rb
@@ -2,11 +2,13 @@ require 'sidekiq-scheduler'
 class SendMailWorker
   include Sidekiq::Worker
 
+  queue_as :mailer
+  
   def perform
     users = User.all
     users.each do |user|
       mail = NotificationMailer.notification(user.id)
-      mail.deliver_now
+      mail.deliver
     end
   end
 end
